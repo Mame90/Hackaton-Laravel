8a557bf07ffb1e51c4bb1f8add5265bf0717f94c
“[mbaye98]” <mbayediop2309481@gmail.com>
2023-10-26 21:20:37 +0200

    avant dernier commit from MbayeDev @

--- resources/views/admin/body.blade.php
+++ resources/views/admin/body.blade.php
@@ -3,8 +3,8 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Espace Admin</h1>
-    <a href="{{url('ajouterProduit')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-            class="fas fa-download fa-sm text-white-50"></i>Ajouter un Utilisateur</a>
+    <a href="{{route('admin.teacher.ajouter')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+            class="fas fa-download fa-sm text-white-50"></i>Ajouter un Professeur</a>
 </div>
 
 <!-- Content Row -->
---
+++ resources/views/voirCours.blade.php
@@ -0,0 +1,181 @@
+<main id="main" class="main">
+
+<div class="pagetitle">
+  <h1>Popular Courses</h1>
+ 
+</div><!-- End Page Title -->
+
+<section class="section dashboard" >
+  <div class="row" >
+
+    <!-- Left side columns -->
+    <div class="col-lg-8">
+      <div class="row">
+
+        <!-- Sales Card -->
+        <div class="card">
+            <div class="card-body" style="margin:0 ;">
+              <h5 class="card-title">With Icon</h5>
+
+
+              <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <div class="row">
+                    <div class="col" style="margin-left:0 ;" >
+                    <img src="assets\img\auth.jpeg" alt="image-ici" width="100px">
+                    </div>
+                    <div class="col-sm " style="margin-left:0 ;" >
+                    <a href="{{ route('mescours') }}" class="card-title">Voir cours</a>
+                    </div>
+                </div>
+               
+               
+              </div>
+              
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <div class="row">
+                    <div class="col" style="margin-left:0 ;" >
+                    <img src="assets\img\auth.jpeg" alt="image-ici" width="100px">
+                    </div>
+                    <div class="col-sm " style="margin-left:0 ;" >
+                    <a href="#" class="card-title">Date Cours</a>
+                    </div>
+                </div>             
+              </div>
+
+              
+              <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <div class="row">
+                    <div class="col" style="margin-left:0 ;" >
+                    <img src="assets\img\auth.jpeg" alt="image-ici" width="100px">
+                    </div>
+                    <div class="col-sm " style="margin-left:0 ;" >
+                    <a href="#" class="card-title">Listes des professeurs</a>
+                    </div>
+                </div>
+              </div>
+
+              <div class="alert alert-dark alert-dismissible fade show" role="alert">
+                <div class="row">
+                    <div class="col" style="margin-left:0 ;" >
+                    <img src="assets\img\auth.jpeg" alt="image-ici" width="100px">
+                    </div>
+                    <div class="col-sm " style="margin-left:0 ;" >
+                    A simple success alert with icon check it out!
+                    </div>
+                </div>
+               
+               
+              </div>
+           
+           
+            </div>
+          </div>
+<!-- End Sales Card -->
+
+        <!-- Revenue Card -->
+    
+        <!-- Customers Card -->
+     
+        <!-- Reports -->
+     
+        <!-- Recent Sales -->
+      
+      </div>
+    </div><!-- End Left side columns -->
+
+    <!-- Right side columns -->
+    <div class="col-lg-4">
+
+      <!-- Recent Activity -->
+      <div class="card">
+        <div class="filter">
+          <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+            <li class="dropdown-header text-start">
+              <h6>Filter</h6>
+            </li>
+
+            <li><a class="dropdown-item" href="#">Today</a></li>
+            <li><a class="dropdown-item" href="#">This Month</a></li>
+            <li><a class="dropdown-item" href="#">This Year</a></li>
+          </ul>
+        </div>
+
+        <div class="card-body">
+          <h5 class="card-title">Recent Activity <span>| Today</span></h5>
+
+          <div class="activity">
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">32 min</div>
+              <i class='bi bi-circle-fill activity-badge text-success align-self-start'></i>
+              <div class="activity-content">
+                Quia quae rerum <a href="#" class="fw-bold text-dark">explicabo officiis</a> beatae
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">56 min</div>
+              <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
+              <div class="activity-content">
+                Voluptatem blanditiis blanditiis eveniet
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">2 hrs</div>
+              <i class='bi bi-circle-fill activity-badge text-primary align-self-start'></i>
+              <div class="activity-content">
+                Voluptates corrupti molestias voluptatem
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">1 day</div>
+              <i class='bi bi-circle-fill activity-badge text-info align-self-start'></i>
+              <div class="activity-content">
+                Tempore autem saepe <a href="#" class="fw-bold text-dark">occaecati voluptatem</a> tempore
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">2 days</div>
+              <i class='bi bi-circle-fill activity-badge text-warning align-self-start'></i>
+              <div class="activity-content">
+                Est sit eum reiciendis exercitationem
+              </div>
+            </div><!-- End activity item-->
+
+            <div class="activity-item d-flex">
+              <div class="activite-label">4 weeks</div>
+              <i class='bi bi-circle-fill activity-badge text-muted align-self-start'></i>
+              <div class="activity-content">
+                Dicta dolorem harum nulla eius. Ut quidem quidem sit quas
+              </div>
+            </div><!-- End activity item-->
+
+          </div>
+
+        </div>
+      </div><!-- End Recent Activity -->
+
+      <!-- Budget Report -->
+    
+      <!-- Website Traffic -->
+     
+      <!-- News & Updates Traffic -->
+    
+    </div><!-- End Right side columns -->
+
+  </div>
+</section>
+
+</main><!-- End #main -->
+<script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
